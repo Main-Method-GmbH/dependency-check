1ca3b45375882370b0a3f1791a69f8c0ed25d5d2
Maik Herrmann <maik.herrmann@dwpbank.de>
2020-07-12 10:59:41 +0200

    New build.sh

--- build.sh
+++ build.sh
@@ -11,5 +11,5 @@ DC_VERSION_FULL=${DC_VERSION_MAJOR_MINOR}.${DC_VERSION_PATCH}
 
 docker login -u ${DOCKER_HUB_USER} -p ${DOCKER_HUB_PASSWORD}
 docker build -t ${IMAGE} -t ${IMAGE}:${DC_VERSION_MAJOR} -t ${IMAGE}:${DC_VERSION_MAJOR_MINOR} -t ${IMAGE}:${DC_VERSION_FULL} .
-docker push ${IMAGE} && docker push ${IMAGE}:${DEPENDENCY_CHECK_VERSION}
+docker push ${IMAGE} && docker push ${IMAGE}:${DC_VERSION_MAJOR} && docker push ${IMAGE}:${DC_VERSION_MAJOR_MINOR} && docker push ${IMAGE}:${DC_VERSION_FULL}
 docker logout
